97711c350492ef78993e8a58e7470c5d8038a674
Andrey <andreii.rubik@mail.ru>
2024-10-21 22:27:31 +0300

    make search form

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from "@/components/shared/_index";
+import { Container, Filters, Title, TopBar } from "@/components/shared/_index";
 
 export default function Home() {
   return (
@@ -6,7 +6,20 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
+
       <TopBar />
+
+      <Container className="mt-10 pb-14 ">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
