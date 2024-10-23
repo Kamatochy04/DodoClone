dee237b413396fb33b560596b740c32d0f99b919
Andrey <andreii.rubik@mail.ru>
2024-10-23 21:16:22 +0300

    commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,22 @@
-import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json({
-    users: ["user1", "user2"],
+export async function GET() {
+  const user = await prisma.user.findMany();
+  return NextResponse.json(user);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      fullName: data.fullName,
+      password: data.password,
+      email: data.email,
+      verified: new Date(),
+    },
   });
+
+  return NextResponse.json(user);
 }
